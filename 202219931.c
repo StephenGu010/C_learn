9c7999a37e789d35a0ff516589beadd1e798105d
Stephen <2316676945@qq.com>
2022-10-09 21:53:14 +0800

    c语言

---
+++ 202219931.c
@@ -0,0 +1,24 @@
+/* c1-1.c */
+#include <stdio.h>
+int main()
+{
+	printf("****************************************************************\n");
+	printf("      _______                                ______             \n");
+	printf("     (  (0)  )                             (  (0)  )            \n");
+	printf("                         ^ o   o ^                              \n");
+	printf("                (                            )                  \n");
+    printf("                                                                \n");
+	printf("                     I I I I I I I I I I                        \n");
+    printf("                       I             I                          \n");
+	printf("                        I I I I I I I           HELLO WORLD     \n");
+    printf("****************************************************************\n");
+	printf("                                                                \n");
+	printf(" *           我是电子科学与技术专业2022141班的学生           *   \n");
+    printf(" *                                                           *   \n");
+	printf(" *                      我叫顾浩民                           *   \n");
+    printf(" *                                                           *   \n");
+	printf(" *                  nice to meet yo ou                       *   \n");
+    printf(" *                                                           *   \n");
+	printf(" *                                 ^                         *   \n");
+	return 0;
+}
