ccd562ba3bbd45b689009f9b04b4683ecf2c64dd
StephenGu010 <105930694+StephenGu010@users.noreply.github.com>
2022-10-23 23:23:30 +0800

    Add files via upload

---
+++ abc.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+char ch;
+if(ch=='一日不读书'||ch=='一日不跑步')
+{
+printf("白 活");
+}
+else {printf("凑合");
+}
+printf("ps:感谢特邀嘉宾董哥@{uin:2529058769,nick:20221996董槟赫,who:1} 陪我夜跑，让我动力十足");
+return 0;
+}
